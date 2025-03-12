60d3251c645c026cbfe5f6b39303be8b60b442db
Raihan Mahardhika <rmahardhikasp1@gmail.com>
2025-03-12 16:28:37 +0700

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,8 @@
+    <!-- Footer -->
+    <footer class="bg-white shadow-lg mt-8">
+        <div class="max-w-6xl mx-auto px-4 py-6">
+            <p class="text-center text-gray-500">&copy; 2023 Marketplace. All rights reserved.</p>
+        </div>
+    </footer>
+</body>
+</html>
